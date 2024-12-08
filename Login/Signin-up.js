65632059f9ed91eddf25421013d4766619b2e942
unejsa.krasniqi <uk70144@ubt-uni.net>
2024-12-08 22:50:33 +0100

    Validimi i SignIn-SignUp

--- Login/Signin-up.js
+++ Login/Signin-up.js
@@ -15,3 +15,27 @@ sign_up_btn2.addEventListener("click", () => {
 sign_in_btn2.addEventListener("click", () => {
     container.classList.remove("sign-up-mode2");
 });
+document.getElementById('sign-up-form').addEventListener('submit', function(event) {
+    let username = document.getElementById('username-signup').value;
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password-signup').value;
+
+    if (username === "" || email === "" || password === "") {
+        alert("Please fill out all fields.");
+        event.preventDefault();
+    }
+
+    let emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    if (!emailPattern.test(email)) {
+        alert("Please enter a valid email address.");
+        event.preventDefault();
+        return;
+    }
+
+   
+    if (password.length < 6) {
+        alert("Password must be at least 6 characters long.");
+        event.preventDefault(); 
+        return;
+    }
+})
